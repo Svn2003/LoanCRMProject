84acabc271cbdf8257a64a5295690b842b6e9e93
Sahana Veerayya Nanjayyanamath <sahanananjayyanamath@gmail.com>
2025-05-21 10:55:59 +0530

    Set host and port for Render deployment

--- app.py
+++ app.py
@@ -48,7 +48,8 @@ def get_all():
 # ✅ Use Render-compatible port and host
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host="0.0.0.0", port=10000)
+    # app.run(host='0.0.0.0', port=port)
 
 
 # from flask import Flask, request, jsonify, render_template
